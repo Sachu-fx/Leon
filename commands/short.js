89a09559ec1232856240fe1be369169dffe54c85
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2023-04-19 16:54:17 +0530

    Add api

--- commands/short.js
+++ commands/short.js
@@ -1,4 +1,5 @@
-let { onCommand, loadLanguage, isURL, shortURL, unshortURL } = require('../main/');
+let { onCommand, loadLanguage, isURL } = require('../main/');
+let api = require('../main/api');
 let { short_desc, unshort_desc, ss_need, invalid_url, already_short, need_short_url, not_short_url } = loadLanguage();
 
 onCommand(
@@ -10,9 +11,9 @@ onCommand(
 
   if (!text[1]) return await msg.reply(ss_need);
   if (!isURL(text[1])) return await msg.reply(ss_invalid);
-  let res = await shortURL(text[1]);
-  if (!res.is_gd) return await msg.reply(already_short);
-  return await msg.reply(res.is_gd);
+  let res = await api.shortURL(text[1]);
+  if (!res) return await msg.reply(already_short);
+  return await msg.reply(res.short_url);
 });
 
 onCommand(
@@ -25,6 +26,6 @@ onCommand(
   if (!text[1]) return await msg.reply(need_short_url);
   if (!isURL(text[1])) return await msg.reply(invalid_url);
   if (!text[1].match(/https?:\/\/is\.gd\/[a-zA-Z0-9]+/)) return await msg.reply(not_short_url);
-  let res = await unshortURL(text[1]);
-  return await msg.reply(res.is_gd);
+  let res = await api.unshortURL(text[1]);
+  return await msg.reply(res.long_url);
 });
