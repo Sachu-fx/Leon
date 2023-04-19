bcc76623b556b864d2a9a9d4f89f53072f4206ab
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2023-04-19 14:41:52 +0530

    APIs

--- main/api.js
+++ main/api.js
@@ -2,23 +2,35 @@ let { fetchJson } = require('./index');
 let config = require('./config');
 
 module.exports = {
- home: 'https://api-toxic-devil.herokuapp.com',
+ home: 'https://toxic-devil-api.onrender.com',
  key: 'B3T4K3Y',
  chatgpt: async (query) => {
   let json = await fetchJson(`${home}/chatgpt?key=${key}&openai_key=${config.OPENAIKEY}&query=${query}`);
+  return json.result;
+ },
+ cat: async (type, say) => {
+  let json = await fetchJson(`${home}/cat?key=${key}&type=${type}${say !== undefined ? '' : '&text='+say}`);
   return json.result;
  },
  dall_e: async (query) => {
   let json = await fetchJson(`${home}/dall-e?key=${key}&openai_key=${config.OPENAIKEY}&query=${query}`);
   return json.result;
  },
- github: async (type, value) => {
-  type = type == 'user' ? 'github_user' : 'github_repo';
-  let json = await fetchJson(`${home}/${type}?key=${key}&${type == 'github_user' ? 'username' : 'repo_url'}=${value}`);
+ emoji_mix: async (emo1, emo2) => {
+  let json = await fetchJson(`${home}/emoji-mix?key=${key}&emoji1=${emo1}&emoji2=${emo2}`);
   return json.result;
  },
  lyrics: async (song) => {
   let json = await fetchJson(`${home}/lyrics?key=${key}&song=${song}`);
   return json.result;
+ },
+ screenshot: async (url) => {
+  let json = await fetchJson(`${home}/screenshot?key=${key}&url=${url}`);
+  return json.result;
+ },
+ wikipedia: async (query) => {
+  let lang = (config.LANG == 'en' || config.LANG == 'eng' || config.LANG == 'english') ? 'en' : (config.LANG == 'ml' || config.LANG == 'mal' || config.LANG == 'malayalam') ? 'ml' : 'en';
+  let json = await fetchJson(`${home}/wikipedia?key=${key}&query=${query}&lang=${lang}`);
+  return json.result;
  }
-}
+};
