d347826df653ebed911a2a0a3269b21dde68f48d
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2023-04-19 14:50:30 +0530

    Wiki api

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,4 +1,5 @@
-let { onCommand, loadLanguage, wikipedia } = require('../main/');
+let { onCommand, loadLanguage } = require('../main/');
+let api = require('../main/api');
 let { wiki_desc, wiki_need, wiki_invalid, wiki_result } = loadLanguage();
 
 onCommand(
@@ -9,8 +10,8 @@ onCommand(
   }, async (msg, text, client) => {
 
   if (!text[1]) return await msg.reply(wiki_need);
-  let wiki = await wikipedia(text[1]), info = '';
+  let wiki = await api.wikipedia(text[1]), info = '';
   if (!wiki) info = wiki_invalid;
-  info = wiki_result.format(wiki.title, wiki.info, encodeURIComponent(wiki.url));
+  info = wiki_result.format(wiki.title, wiki.info, wiki.url);
   return await msg.reply(info);
 });
