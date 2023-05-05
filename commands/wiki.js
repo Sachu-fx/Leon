9aa2d33de04264c9136b1e0808f8922a7dd74806
TOXIC DEVIL <78701752+TOXIC-DEVIL@users.noreply.github.com>
2023-05-05 20:51:41 +0530

    Update wiki.js

--- commands/wiki.js
+++ commands/wiki.js
@@ -1,6 +1,6 @@
 let { onCommand, loadLanguage } = require('../main/');
 let api = require('../main/api');
-let { wiki_desc, wiki_need, wiki_invalid, wiki_result } = loadLanguage();
+let { wiki_desc, planetary_desc, wiki_need, wiki_invalid, wiki_result } = loadLanguage();
 
 onCommand(
   {
@@ -14,4 +14,18 @@ onCommand(
   if (!wiki) info = wiki_invalid;
   info = wiki_result.format(wiki.title, wiki.info, wiki.url);
   return await msg.reply(info);
+});
+
+onCommand(
+  {
+   command: 'planetary',
+   desc: planetary_desc,
+   category: ['misc']
+  }, async (msg, text, client) => {
+
+  let planetary = await api.planetary();
+  let url = planetary.image, caption = '*' + planetary.title + '*\n\n```' + planetary.explanation + '```';
+  await client.sendReply({ type: 'image', message: { url }, caption }).then(() => true)
+   .catch(async (_) => await msg.reply(caption)
+  );
 });
